9f410bb98b1087fe8f3a97a26757df66096114bd
Jun Han <junhan@microsoft.com>
2017-04-25 13:59:42 +0800

    Add navigation bar and use vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand">Azure IoT Diagnostics</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li><router-link to="/" exact>Home</router-link></li>
+          <li><router-link to="/test" exact>Link2</router-link></li>
+          <li><router-link to="#" exact>Link3</router-link></li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +23,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.navbar {
+  border-radius: 0;
+}
+
+.navbar-inverse .navbar-nav .active {
+  color: #fff;
+  background-color: #080000;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
     }
-  ]
+  ],
+  linkActiveClass: 'active'
 })
